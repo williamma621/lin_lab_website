5f56a6763f96254800d94726ff990e58b87381eb
williamma621 <58480359+williamma621@users.noreply.github.com>
2025-07-03 22:03:52 +0800

    Update script.js to be modulized under class FileManager

--- script.js
+++ script.js
@@ -1,57 +1,84 @@
+class bflowData {
+    constructor(text){
+        const lines = text.split(/\r\n|\n/);
+        this.data = [];
+        let i = 0; while (lines[i] != "No: 	Hour 	Min 	Sec 	F1 ") i++;
+        for (i += 1; i < lines.length - 1; i++) this.data.push(lines[i].split("\t")[4])
+    }
+}
+class eflowData {
+    constructor(text){
+        const lines = text.split(/\r\n|\n/);
+        this.data = [];
+        let i = 0; while (lines[i] != "No,Time (hr:min:sec),LeftLU,Event messages") i++;
+        for (i += 1; lines[i]; i++) this.data.push(lines[i].split(",")[2])
+    }
+}
+class FileDataManager {
+    constructor() {
+        this.eflow = null;
+        this.bflow = null;
+    }
+
+    readFromInput(event, key) {
+        const reader = new FileReader();
+        reader.onload = () => { 
+            if (key == 'bflow') this.bflow = new bflowData(reader.result) 
+            if (key == 'eflow') this.eflow = new eflowData(reader.result)
+        };
+        reader.readAsText(event.target.files[0]);
+    }
+}
+
+
+
 let bflow = document.getElementById("b-flow")
 let eflow = document.getElementById("e-flow")
 let button1 = document.getElementById("generate-graph")
-let textarea = document.querySelector('textarea');
-button1.addEventListener('click', ()=> {
-    const bflow_file = bflow.files[0];
-    const eflow_file = eflow.files[0];
-    const bflowReader = new FileReader();
-    const eflowReader = new FileReader();
-    bflowText = false
-    eflowText = false;
-    bflowReader.onload = () => {
-        bflowText = bflowReader.result;
-        blines = bflowText.split(/\r\n|\n/);
-        plotGraph(); // check if both done
-    };
 
-    eflowReader.onload = () => {
-        eflowText = eflowReader.result;
-        elines = eflowText.split(/\r\n|\n/);
-        plotGraph(); // check if both done
-    };
-
-    bflowReader.readAsText(bflow_file);
-    eflowReader.readAsText(eflow_file);
-
-});
+const manager = new FileDataManager();
+bflow.addEventListener('change', (e) => manager.readFromInput(e, 'bflow'));
+eflow.addEventListener('change', (e) => manager.readFromInput(e, 'eflow'));
+button1.addEventListener('click', plotGraph());
 
 
-function maybeDisplay() {
-  if (bflowText && eflowText) {
-    textarea.value = `Blood Flow:\n${bflowText}\n\nElectric Flow:\n${eflowText}`;
-  }
+
+function calculate_basic_stats(data){
+    const lines = [];
+
+    for (let i = 45; i < 2049; i++){
+        const row = data[i].split("\t");
+        const timestamp = `${row[1]}:${row[2]}:${row[3]}`;
+        lines.push(parseFloat(row[4]));
+    }
+    part1_begin = 0
+    part1_end = 120
+    part1_slice = lines.slice(part1_begin, part1_end)
+    part1_mean = ss.mean(part1_slice)
+    part1_sd = ss.standardDeviation(part1_slice)
+
+    part2_begin = 121
+    part2_end = 240
+    part2_slice = lines.slice(part2_begin, part2_end)
+    part2_mean = ss.mean(part2_slice)
+    part2_sd = ss.standardDeviation(part2_slice)
+
+    const tTestResult = ss.tTestTwoSample(part1_slice, part2_slice);
+    console.log("Part1 Mean:", part1_mean);
+    console.log("Part1 SD:", part1_sd);
+    console.log("Part2 Mean:", part2_mean);
+    console.log("Part2 SD:", part2_sd);
+
+    console.log("t-stats: ", tTestResult);
 }
 
 function plotGraph(){
-if (bflowText && eflowText){
-const labels = [];
-const bloodData = [];
-const electricData = [];
 
-for (let i = 45; i < 2049; i++){
-const row = blines[i].split("\t");
-const timestamp = `${row[1]}:${row[2]}:${row[3]}`;
-labels.push(timestamp);
-bloodData.push(parseFloat(row[4]));
-}
-
-for (let i = 21; i < 2035; i++){
-    const row = elines[i].split(",");
-    electricData.push(parseFloat(row[2]));
-}
-
+const bloodData = manager.bflow.data
+const electricData = manager.eflow.data
 const ctx = document.getElementById('combined-chart').getContext('2d');
+const labels = []
+for (let i = 0; i< 2099; i++){labels.push(i)}
 
 new Chart(ctx, {
 type: 'line',
@@ -105,4 +132,4 @@ options: {
 
 
 }
-}
+
